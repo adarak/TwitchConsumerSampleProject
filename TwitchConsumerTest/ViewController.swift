4fbd1fe99294cdc5474eb1b0050aebefdf10c33b
Adam Ake <adam@weareenvoy.com>
2018-01-07 12:04:22 -0800

    Initial Commit

---
+++ TwitchConsumerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitchConsumerTest
+//
+//  Created by Envoy on 1/7/18.
+//  Copyright © 2018 Adam Ake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
